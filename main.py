4f5ee60ff7075da6e74a9124a01759add2ea4994
WaHaHaAHaaHa <hehaojia1016@163.com>
2023-09-11 11:36:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ def proxy(path):
     url = f"https://{OPENAI_API_HOST}/{path}"
     headers = request.headers
     data = request.get_data()
-    
+
     response = requests.request(
         request.method, url, headers=headers, data=data)
     return response.content, response.status_code, response.headers.items()
