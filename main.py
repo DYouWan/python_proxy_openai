f1a743e29ce0b522de50b9947e19a6fc2ea8efa9
WaHaHaAHaaHa <hehaojia1016@163.com>
2023-09-11 09:44:31 +0800

    Create main.py
    init

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+OPENAI_API_HOST = "api.openai.com"
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def proxy(path):
+    url = f"https://{OPENAI_API_HOST}/{path}"
+    headers = request.headers
+    data = request.get_data()
+    response = requests.request(
+        request.method, url, headers=headers, data=data)
+    return response.content, response.status_code, response.headers.items()
+
+
+if __name__ == '__main__':
+    app.run()
