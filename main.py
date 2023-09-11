18788a5846c97a2185a86f67c074ee57145de1e7
WaHaHaAHaaHa <hehaojia1016@163.com>
2023-09-11 16:57:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,24 +26,9 @@ def proxy(path):
             key, value) in request.headers.items() if key != 'Host'}
 
         data = request.get_data()
-        response = requests.request(
+        return requests.request(
             request.method, url, headers=headers, data=data, allow_redirects=False
         )
-
-        app.logger.info(response.headers.get("Content-Encoding"))
-        app.logger.info(response.content)
-
-        # 检查响应的内容类型是否为 JSON
-        if response.headers.get("Content-Type") == "application/json":
-            # 使用原始数据
-            uncompressed_data = response.content
-        else:
-            # 其他情况（如 gzip 压缩），使用原始数据或进行相应处理
-            uncompressed_data = response.content
-
-        # 将 OpenAI API 的响应转发给客户端
-        return Response(uncompressed_data, response.status_code, headers=response.headers.items())
-
-
+        
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
